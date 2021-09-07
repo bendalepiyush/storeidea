68bdc9d1beb55dfb85b70c71c5f60dbc9d9501db
Piyush Bendale <bendalepiyush73@gmail.com>
2021-09-08 01:12:28 +0530

    Commit

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,22 +1,51 @@
 import styled from 'styled-components'
+import { FaStore, FaTag, FaCog } from 'react-icons/fa'
 
-const MenuItem = styled.div`
-    padding-left: 18px;
+const Body = styled.div`
+    padding: 50px 0;
+`
+
+const ActiveMenuItem = styled.div`
+    padding-left: 25px;
     border-left: 2px solid var(--primary);
     height: 60px;
     background-color: rgba(123, 31, 162, 0.05);
     display: flex;
     align-items: center;
+
+    & span {
+        margin-left: 15px;
+        color: var(--primary);
+        font-weight: 500;
+    }
+`
+
+const MenuItem = styled.div`
+    padding-left: 25px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+
+    & span {
+        margin-left: 15px;
+        font-weight: 500;
+    }
 `
 
 const Sidebar = () => {
 
     return (
-        <div>
+        <Body>
             <MenuItem>
-                <span>Dashboard</span>
+                <FaStore/> <span> Dashboard</span>
             </MenuItem>
-        </div>
+            <ActiveMenuItem>
+                <FaTag color="var(--primary)"/> <span> Products</span>
+            </ActiveMenuItem>
+            <MenuItem>
+            <FaCog/> <span> Settings</span>
+            </MenuItem>
+        </Body>
     )
 }
 
